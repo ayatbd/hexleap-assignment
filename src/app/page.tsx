8fa4007feeb7e66c1dada3f2d85f2da0983c1d85
A Behesti <albehestione@gmail.com>
2024-03-26 10:56:45 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Sports from "@/components/Sports/Sports";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Sports></Sports>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/Sports/Sports.tsx
@@ -0,0 +1,38 @@
+
+
+const Sports = () => {
+    const data = [
+        {
+            "id": "1",
+            "image": "",
+            "heading": "",
+            "total_events": "",
+            "sport": ""
+        }
+    ]
+    return (
+        <div>
+            <p>Sports</p>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <h4></h4>
+                        <div>
+                            <div>
+                                <p></p>
+                                <p></p>
+                            </div>
+                            <div>
+                                <p></p>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Sports;
